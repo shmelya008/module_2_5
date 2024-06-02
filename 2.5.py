af2e67c4dc02660844af689b34393be8d4dcdd27
shmelya008 <shmelya008@gmail.com>
2024-06-02 22:09:08 +0300

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -1,11 +1,22 @@
 def get_matrix(n, m, value):
     matrix = []
-
     for i in range(n):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-    print(matrix)
+    return matrix
 
-get_matrix(2, 2, 42)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+# Вывод:
+# [[10, 10], [10, 10]]
+# [[42, 42, 42, 42, 42], [42, 42, 42, 42, 42], [42, 42, 42, 42, 42]]
+# [[13, 13], [13, 13], [13, 13], [13, 13]]
+
+# Process finished with exit code 0
 
