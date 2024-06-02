731eee168824487bc55d7b27e069d4c750980213
shmelya008 <shmelya008@gmail.com>
2024-06-02 21:49:54 +0300

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -1,16 +1,11 @@
 def get_matrix(n, m, value):
     matrix = []
-    a = []
-    b = []
-    # n = 4
-    # m = 2
-    # value = 42
+
     for i in range(n):
-        matrix.append(a)
+        matrix.append([])
         for j in range(m):
-            a.append(value)
-            break
-        print(matrix)
+            matrix[i].append(value)
+    print(matrix)
 
 get_matrix(2, 2, 42)
-get_matrix(3,1,52)
+
