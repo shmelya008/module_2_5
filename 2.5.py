f795c1524c6433adda88975236f931befac81074
shmelya008 <shmelya008@gmail.com>
2024-06-02 20:28:11 +0300

    Add files via upload

---
+++ 2.5.py
@@ -0,0 +1,16 @@
+def get_matrix(n, m, value):
+    matrix = []
+    a = []
+    b = []
+    # n = 4
+    # m = 2
+    # value = 42
+    for i in range(n):
+        matrix.append(a)
+        for j in range(m):
+            a.append(value)
+            break
+        print(matrix)
+
+get_matrix(2, 2, 42)
+get_matrix(3,1,52)
